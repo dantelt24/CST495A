dc14d294d48efa71426c3033e227df7d9560455f
dantelt24 <dlacey-thompson@csumb.edu>
2017-11-08 16:14:52 -0800

    Updating for the changes from going api v1 to api v2

--- KantoPokedex/PokemonExtension.swift
+++ KantoPokedex/PokemonExtension.swift
@@ -12,7 +12,7 @@ extension Pokemon: PokemonProtocol{
     var nextPokeID: Int! {return self.pkid + 1}
     func deletePokemon() {} // optional pokemon protocol function
     func pokeDetails() -> String {
-        let details: String = "The pokemon's name is \(name!). It is described as \(description!) with a weight of \(weight!) lbs and a height of \(height!). It is a \(type!) type pokemon and has an attack value of \(attack!) and defense value of \(defense!)."
+        let details: String = "The pokemon's name is \(name!). It is described as having a weight of \(weight!) lbs and a height of \(height!). It is a \(type!) type pokemon and has an attack value of \(attack!) and defense value of \(defense!)."
         return details
     }
 }
